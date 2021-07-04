baf7690ad8c7f8f4458e869a4ebdbcad69aafc28
Rodrigo Moreira <hola@piducan.dev>
2021-07-03 21:36:55 -0400

    enable cors

--- api/index.js
+++ api/index.js
@@ -1,11 +1,23 @@
 const { appendToSpreadsheet } = require("../utils/spreadsheets");
 
-module.exports = async (req, res) => {
+const handler = async ({ spreadsheetId, range, values }, res) => {
   try {
-    const { spreadsheetId, range, values } = JSON.parse(req.body);
-    await appendToSpreadsheet({ spreadsheetId, range, values });
-    res.status(200).json({ data: "success" });
+    const { status, statusText } = await appendToSpreadsheet({ spreadsheetId, range, values });
+    res.json({ status, statusText });
   } catch (error) {
     res.status(500).json({ error });
   }
 };
+
+// Enable cors https://vercel.com/support/articles/how-to-enable-cors
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS,POST");
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+module.exports = allowCors(handler);
