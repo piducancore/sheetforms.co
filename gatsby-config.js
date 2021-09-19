61f7b95bb73d804e2be229efce0df8baefd98535
piducancore:/# <hola@piducan.dev>
2021-09-19 10:56:53 -0300

    install gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: "sheetforms.co",
+    siteUrl: "https://sheetforms.co",
+    description: "A simple API to append data to a Google spreadsheet. Just point your forms at it.",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+
+    {
+      resolve: "gatsby-plugin-theme-ui",
+      options: {
+        prismPreset: "night-owl",
+        preset: "@theme-ui/preset-deep",
+      },
+    },
+  ],
+};
