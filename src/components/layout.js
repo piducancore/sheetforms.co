bbb78a864ca7799d10a57ca3f9cccd5e27a8cd98
piducancore:/# <hola@piducan.dev>
2021-09-19 14:42:33 -0300

    update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,70 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { useStaticQuery, graphql } from "gatsby";
+
+import { BaseStyles } from "theme-ui";
+
+const Container = (props) => (
+  <div
+    {...props}
+    sx={{
+      maxWidth: "container",
+      mx: "auto",
+      px: 3,
+    }}
+  />
+);
 
 export default function Layout({ children }) {
-  return <div>{children}</div>;
+  const {
+    site: { siteMetadata },
+  } = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  return (
+    <BaseStyles>
+      <div
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+          variant: "layout.root",
+        }}
+      >
+        <header
+          sx={{
+            width: "100%",
+            variant: "layout.header",
+          }}
+        >
+          <Container>
+            <h1>{siteMetadata.title}</h1>
+          </Container>
+        </header>
+        <main
+          sx={{
+            width: "100%",
+            flex: "1 1 auto",
+            variant: "layout.main",
+          }}
+        >
+          <Container>{children}</Container>
+        </main>
+        <footer
+          sx={{
+            width: "100%",
+            variant: "layout.footer",
+          }}
+        >
+          <Container>{siteMetadata.title}</Container>
+        </footer>
+      </div>
+    </BaseStyles>
+  );
 }
