db87ea6d13a1de24b9d998c9f7eaa694ee05940b
piducancore:/# <hola@piducan.dev>
2021-10-26 12:05:25 -0300

    add styles

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,69 +1,45 @@
-import nightOwl from "@theme-ui/prism/presets/night-owl.json";
+import { system } from "@theme-ui/presets";
+import { merge } from "theme-ui";
+import prismPreset from "@theme-ui/prism/presets/night-owl.json";
 
 import "@fontsource/space-grotesk/300.css";
 import "@fontsource/space-grotesk/500.css";
 import "@fontsource/space-grotesk/700.css";
 
-const theme = {
-  breakpoints: ["40em", "52em", "64em"],
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+const theme = merge(system, {
   fonts: { body: "Space Grotesk", heading: "Space Grotesk", monospace: "monospace" },
+  sizes: { container: 768 },
   fontSizes: [14, 16, 20, 24, 32, 48, 64, 96, 128],
+
   fontWeights: {
-    body: 300,
+    body: 500,
     heading: 700,
-    bold: 500,
-  },
-  lineHeights: {
-    body: 1.5,
-    heading: 1.125,
+    bold: 700,
   },
   colors: {
-    text: "#000",
-    background: "#fff",
-    primary: "#07c",
-    secondary: "#30c",
-    muted: "#f6f6f6",
-  },
-  text: {
-    heading: {
-      fontFamily: "heading",
-      lineHeight: "heading",
-      fontWeight: "heading",
-    },
-  },
-  sizes: { container: 768 },
-  buttons: {
-    primary: {
-      border: (theme) => `1px solid ${theme.colors.primary}`,
-      borderRadius: 4,
-      cursor: "pointer",
-      bg: "primary",
-      color: "background",
-      "&:hover": {
-        bg: "background",
-        color: "primary",
-      },
-    },
+    // background: "#bbb",
+    // primary: "#ff79c6",
+    // // background: "##f0f5fa",
+    // text: "#222639",
   },
   styles: {
-    root: { fontFamily: "body", lineHeight: "body", fontWeight: "body" },
-    h1: { variant: "text.heading", fontSize: 5 },
-    h2: { variant: "text.heading", fontSize: 4 },
-    h3: { variant: "text.heading", fontSize: 3 },
-    h4: { variant: "text.heading", fontSize: 2 },
-    h5: { variant: "text.heading", fontSize: 1 },
-    h6: { variant: "text.heading", fontSize: 0 },
-    p: { fontSize: 3 },
-    // pre: { p: 2, borderRadius: 4, fontFamily: "monospace", overflowX: "auto" },
-    code: { borderRadius: 4, border: 0, ...nightOwl },
+    a: {
+      textDecoration: "none",
+      "&:hover": {
+        color: "yellow",
+      },
+    },
+    iframe: { borderRadius: 4 },
     form: { fontFamily: "body", label: { fontWeight: "bold" }, input: { mb: 3 }, textarea: { mb: 3 } },
-    table: { width: "100%", borderCollapse: "separate", borderSpacing: 0 },
-    th: { textAlign: "left", borderBottomStyle: "solid" },
-    td: { textAlign: "left", borderBottomStyle: "solid" },
-    iframe: { borderRadius: 4, border: "none" },
+    code: {
+      ...prismPreset,
+      p: 1,
+      borderRadius: 4,
+    },
+    pre: {
+      ...prismPreset,
+    },
   },
-  layout: { header: {}, main: {}, footer: {} },
-};
+});
 
 export default theme;
